4500234e509057cfe03bf668d654fcd4e7076965
Artur <artur_master13@mail.ru>
2025-05-29 13:26:00 +0300

    html+css

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="wrapper">
+    <header>
+      <h2>Playable PIANO</h2>
+      <div class="column volume-slider">
+        <span>Volume</span>
+        <input type="range" class="slider">
+      </div>
+      <div class="column keys-checkbox">
+        <span>show keys</span>
+        <input type="checkbox">
+      </div>
+    </header>
+    <ul class="piano-keys">
+      <li class="key white"><span>a</span></li>
+      <li class="key black"><span>w</span></li>
+      <li class="key white"><span>s</span></li>
+      <li class="key black"><span>e</span></li>
+      <li class="key white"><span>d</span></li>
+      <li class="key white"><span>f</span></li>
+      <li class="key black"><span>t</span></li>
+      <li class="key white"><span>g</span></li>
+      <li class="key black"><span>y</span></li>
+      <li class="key white"><span>h</span></li>
+      <li class="key black"><span>u</span></li>
+      <li class="key white"><span>j</span></li>
+      <li class="key white"><span>k</span></li>
+      <li class="key black"><span>o</span></li>
+      <li class="key white"><span>l</span></li>
+      <li class="key black"><span>p</span></li>
+      <li class="key white"><span>;</span></li>
+      
+    </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
